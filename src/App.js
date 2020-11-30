c2639cc87b4be55665a5ec5ae8f439802e9485a6
Valdelar <valdelar.martins@gmail.com>
2020-11-30 16:48:17 -0300

    Add service to retrieve products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import Header from './components/Header';
+import Main from './pages/main'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header />  
+      <Main />
+
     </div>
   );
 }
