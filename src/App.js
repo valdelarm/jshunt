5a3e2936e57c2aad97f521ca8b4dc23d82f92fee
Valdelar <valdelar.martins@gmail.com>
2020-11-30 16:23:52 -0300

    Add newHeader component and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React, {Component} from "react";
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello, world</h1>
+      <Header />
     </div>
   );
 }
